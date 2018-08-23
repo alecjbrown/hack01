2963e81ce680d2283a0962745137bb233666fa9d
alecjbrown <42518203+alecjbrown@users.noreply.github.com>
2018-08-23 14:29:12 -0500

    Added File to master branch
    Text print program

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Alec Brown
+ * Date: 2018/08/22
+ *
+ * A simple Text print program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("I am Alec Brown and I am an Electrical Engineering Major.\n");
+
+  return 0;
+}
